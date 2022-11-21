7bbd5e0127715c22de0db0e541b7aef2485f12ab
Maria2334 <117980313+Maria2334@users.noreply.github.com>
2022-11-21 10:48:19 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,15 @@
+﻿begin
+var a:array [1..20] of integer;
+var y,v:integer;
+writeln('Введите массив');
+for var i:=1 to 20 do
+a[i]:=ReadInteger;
+for var i:=1 to 19 do
+if a[i]>=a[i+1] then 
+y:=y+1;
+for var i:=1 to 19 do
+if a[i]<=a[i+1] then 
+v:=v+1;
+if (y=19) or (v=19) then writeln('Массив монотонный')
+else writeln('Массив не монотонный');
+end.
---
+++ 2.pas
@@ -0,0 +1,21 @@
+﻿const 
+alf=['A'..'Z','a'..'z','А'..'Я','а'..'я'];
+begin
+var s:string;
+writeln('Введите строку');
+s:=Readstring;
+var d:integer;
+d:=length(s);
+var p,b:integer;
+for var i:=1 to d do
+begin
+if s[i]=' ' then 
+p:=p+1;
+end;
+for var i:=1 to d do
+begin
+if s[i] in alf then
+b:=b+1;
+end;
+writeln('Число слов в строке: ',p+1,' число букв в строке: ',b);
+end.
